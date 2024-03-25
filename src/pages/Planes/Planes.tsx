9c6a7ccd15111b09e625ea781a37cf9a94506bce
muradnuriyev001 <muradnuriyev001@gmail.com>
2024-03-25 20:15:09 +0400

    Added array which contains manually added images of planes for PlaneCard section, because of errors with path in planeData.json(check .copy.json)

--- src/pages/Planes/Planes.tsx
+++ src/pages/Planes/Planes.tsx
@@ -7,6 +7,35 @@ import HomeComments from "../Home/HomeComments/HomeComments";
 import HomeOffer from "../Home/HomeOffer/HomeOffer";
 import usePageTitle from "../../hooks/usePageTitle";
 
+//Bad method but there are errors in deployment from planeData.json like "image":url
+import plane1 from "../../assets/img/Plane/Models/plane-1.jpg";
+import plane2 from "../../assets/img/Plane/Models/plane-2.jpg";
+import plane3 from "../../assets/img/Plane/Models/plane-3.jpg";
+import plane4 from "../../assets/img/Plane/Models/plane-4.jpg";
+import plane5 from "../../assets/img/Plane/Models/plane-5.jpg";
+import plane6 from "../../assets/img/Plane/Models/plane-6.jpg";
+import plane7 from "../../assets/img/Plane/Models/plane-7.jpg";
+import plane8 from "../../assets/img/Plane/Models/plane-8.jpg";
+import plane9 from "../../assets/img/Plane/Models/plane-9.jpg";
+import plane10 from "../../assets/img/Plane/Models/plane-10.jpg";
+import plane11 from "../../assets/img/Plane/Models/plane-11.jpg";
+import plane12 from "../../assets/img/Plane/Models/plane-12.jpg";
+
+const planeImages = [
+  plane1,
+  plane2,
+  plane3,
+  plane4,
+  plane5,
+  plane6,
+  plane7,
+  plane8,
+  plane9,
+  plane10,
+  plane11,
+  plane12,
+];
+
 const Planes = () => {
   usePageTitle("AirLine Jet | Planes");
   return (
@@ -15,7 +44,7 @@ const Planes = () => {
       <h1>Explore our planes</h1>
       <div className={c.planes}>
         {planesData.map((plane, i) => (
-          <PlaneCard key={i} {...plane} />
+          <PlaneCard key={i} {...plane} image={planeImages[i]} />
         ))}
       </div>
       <HomeOffer />
