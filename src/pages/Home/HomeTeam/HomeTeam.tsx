cc5d627f9f840c935886da0584898fe126e87b6a
muradnuriyev001 <muradnuriyev001@gmail.com>
2024-03-29 17:25:15 +0400

    Style fix #15, netlify images not load fix test

--- src/pages/Home/HomeTeam/HomeTeam.tsx
+++ src/pages/Home/HomeTeam/HomeTeam.tsx
@@ -1,19 +1,19 @@
 import c from "./HomeTeam.module.scss";
 
 //image imports
-import member1 from "../../../assets/img/Home/Team/member1.png";
-import member2 from "../../../assets/img/Home/Team/member2.jpg";
-import member3 from "../../../assets/img/Home/Team/member3.png";
+import member1 from "../../../assets/img/Home/Team/member1.jpg";
+import member2 from "../../../assets/img/Home/Team/member3.png";
+import member3 from "../../../assets/img/Home/Team/member3.jpg";
 import member4 from "../../../assets/img/Home/Team/member4.jpg";
 import HomeTeamCard from "./HomeTeamCard/HomeTeamCard";
 
-const membersImages = [member1, member2, member3, member4];
+const membersImages = [member1, member3, member2, member4];
 
 //MembersInfo
 
 const membersInfo = [
   { name: "Adam Smith", role: "Pilot" },
-  { name: "Benjamin Johnson", role: "Pilot" },
+  { name: "Jasmin White", role: "Pilot" },
   { name: "Emily Brown", role: "Crew" },
   { name: "Sophia Martinez", role: "Crew" },
 ];
